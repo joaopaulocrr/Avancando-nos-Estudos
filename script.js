4d2715abbaeb49e5cd9436b9e551a309a295f3e8
João Paulo <paulocorrentinense@gmail.com>
2023-03-16 17:43:54 -0300

    ..

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //alert('Olá, seja bem vindo ao Javascript!');
 //confirm('Você vai conhecer um mundo sem limites!')
-let nome = prompt('Qual é o seu nome? ')
-let sal = prompt('Qual é o seu salário? ')
-alert(`Seja bem-vindo(a) ${nome}, seu salário atual é R$ ${sal}`)
+//let nome = prompt('Qual é o seu nome? ')
+//let sal = prompt('Qual é o seu salário? ')
+let msg = document.getElementsByTagName('p')[0]
+msg.innerText = 'Seja bem-vindo(a), seu salário atual é R$ '
+msg.style.background = 'green'
+
+
