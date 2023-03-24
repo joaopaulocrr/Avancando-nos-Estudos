77fa3ba1e2784462720138ae353e57de18056f86
João Paulo <paulocorrentinense@gmail.com>
2023-03-24 13:21:35 -0300

    v.4

--- condicoes-multiplas/script.js
+++ condicoes-multiplas/script.js
@@ -9,41 +9,49 @@ function qualDia() {
         case "domingo":
             resu.innerHTML += "<p>Dia de FUTEBOL.</p>"
             document.body.style.backgroundImage = "url('imagens/futebol.jpg')" ;
+            document.body.style.color = 'white'
             break;
 
         case "segunda":
             resu.innerHTML += "<p>Dia de ESTUDAR.</p>"
             document.body.style.backgroundImage = "url('imagens/estudos.jpg')" ;
+            document.body.style.color = 'black'
             break;
             
         case "terça":
             resu.innerHTML += "<p>Dia de IGREJA.</p>"
             document.body.style.backgroundImage = "url('imagens/igreja.jpg')" ;
+            document.body.style.color = 'white'
             break;
 
         case "quarta":
             resu.innerHTML += "<p>Dia de CINEMA.</p>"
             document.body.style.backgroundImage = "url('imagens/cinema.jpg')" ;
+            document.body.style.color = 'white'
             break;
 
         case "quinta":
             resu.innerHTML += "<p>Dia de TRABALHAR.</p>"
-            document.body.style.backgroundImage = "url('imagens/trabalho.jpg')" ;
+            document.body.style.backgroundImage = "url('imagens/trabalho.jpg'" ;
+            document.body.style.color = 'white'
             break;
 
         case "sexta":
             resu.innerHTML += "<p>Dia dos AMIGOS.</p>"
             document.body.style.backgroundImage = "url('imagens/amigos.jpg')" ;
+            document.body.style.color = 'white'
             break;
 
         case "sábado":
             resu.innerHTML += "<p>Dia de FAMÍLIA.</p>"
             document.body.style.backgroundImage = "url('imagens/familia.jpg')" ;
+            document.body.style.color = 'black'
             break;
 
         default:
             resu.innerHTML = '[ERRO] Dia inválido'
-            document.body.style.backgroundImage = "url('imagens/.jpg')" ;
+            document.body.style.backgroundImage = "url('imagens/erro.jpg')" ;
+            document.body.style.color = 'white'
             break;
     }
 }
